9cca828b20c08e740ba26b3f22ef1b5a2e61ffb4
Jan Maly <maly@kurzor.net>
2022-01-11 20:48:08 +0100

    Working github fetcher with scroll

--- github-starred/src/App.svelte
+++ github-starred/src/App.svelte
@@ -1,61 +1,90 @@
 <script>
-	import { onMount } from "svelte";
-	import Repo from './Repo.svelte';
-	import { createRepo } from './utils';
-	
-	let repos = [];
-	let loading = false;
-	
-	const updateRepos = async () => {
-		loading = true;
-		const res = await fetch(`https://api.github.com/search/repositories?q=created:>2022-01-04&sort=stars&order=desc`);
-		const result_object = await res.json();
-		repos = result_object.items.map( item => createRepo(item));		
-		loading = false;
-	};
+  import { onMount } from "svelte";
+  import Repo from "./Repo.svelte";
+  import { createRepo, getLastWeekDate } from "./utils";
 
-	onMount(updateRepos);
+  let repos = [];
+  let newBatch = [];
+  let loading = false;
+  let error = null;
+  let page = 1;
+
+  const clearRepos = async () => {
+    repos = [];
+    newBatch = [];
+    page = 1;
+    await moreRepos();
+  };
+
+  const moreRepos = async () => {
+    loading = true;
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=created:>=${getLastWeekDate()}&sort=stars&order=desc&per_page=9&page=${page}`
+    );
+    if (!response.ok) {
+      const errObj = await response.json();
+      error = `Error occured while loading: ${response.status} - ${errObj.message}`;
+    } else {
+      const result_object = await response.json();
+      newBatch = result_object.items.map((item) => createRepo(item));
+      page += 1;
+      error = null;
+    }
+    loading = false;
+  };
+
+  $: repos = [...repos, ...newBatch];
+
+  onMount(moreRepos);
 </script>
 
 <main>
-	<h1>Trending Repos in GitHub</h1>
-	<h2>Last week statistics</h2>	
-	{#if loading}
-		<div class="loading">loading...</div>
-	{:else}
-		<div class="repos">
-			{#each repos as repo (repo.id)} 		
-				<Repo {...repo} />
-			{/each}
-		</div>
-	{/if}
+  <h1>Trending Repos in GitHub</h1>
+  <h2>Last week statistics</h2>
+  <p>
+    <a href="#reload" on:click|preventDefault={clearRepos}>start from scratch</a
+    >
+  </p>
+  {#if loading}
+    <div class="loading">loading...</div>
+  {:else if error}
+    <div class="error">{error}</div>
+  {:else}
+    <div class="repos">
+      {#each repos as repo (repo.id)}
+        <Repo {...repo} />
+      {/each}
+    </div>
+    <p><a href="#loadmore" on:click|preventDefault={moreRepos}>load more</a></p>
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-
-	.repos {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-evenly;
-	}
-
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  .error {
+    color: red;
+  }
+  .repos {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
 </style>
--- github-starred/src/utils.js
+++ github-starred/src/utils.js
@@ -1,3 +1,17 @@
-export const createRepo = ({ html_url: url, name, id, stargazers_count: stars, description }) => {
+import { formatDate, toDate } from "date-utils-2020";
+
+export const getLastWeekDate = () => {
+  let d = new Date();
+  d.setDate(d.getDate() - 7);
+  return formatDate(d, "yyyy-MM-dd");
+};
+
+export const createRepo = ({
+  html_url: url,
+  name,
+  id,
+  stargazers_count: stars,
+  description,
+}) => {
   return { url, name, id, stars, description };
 };
